5edac779e3deb254d00330c4f5c229e3d7a4dab6
Antonio Campos <campos.antonio@aluno.ifsp.edu.br>
2025-07-14 08:46:02 -0300

    refac: adequações de layout

--- resources/views/documento/show.blade.php
+++ resources/views/documento/show.blade.php
@@ -26,7 +26,9 @@
           <form action="{{ route('documento.finalizar', $documento) }}" method="POST" class="d-inline">
             @csrf
             @method('PATCH')
-            <button type="submit" class="btn btn-warning"
+            <button type="submit" class="btn btn-warning" 
+              title="Ao finalizar, este documento será marcado como concluído e não poderá mais ser editado. 
+              Certifique-se de que todas as informações estão corretas antes de prosseguir."
               onclick="return confirm('Tem certeza que deseja finalizar este documento?')">
               <i class="fas fa-check"></i> Finalizar
             </button>
